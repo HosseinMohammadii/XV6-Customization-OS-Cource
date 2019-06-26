35f6933689d7ba48bf37d7e45677ef7045d3fac4
Hossein <hossein.slayer@gmail.com>
2019-06-26 13:47:46 +0430

    add c,r,etime

--- proc.h
+++ proc.h
@@ -49,6 +49,10 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int ctime;                   // Process creation time
+  int etime;                   // Process end time
+  int rtime;                   // Process total running time
+
 };
 
 // Process memory is laid out contiguously, low addresses first:
--- user.h
+++ user.h
@@ -24,6 +24,7 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 int getppid(void);
+int getPerformanceData(int *wtime, int *rtime);
 
 // ulib.c
 int stat(const char*, struct stat*);
